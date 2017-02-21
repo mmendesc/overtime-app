295d2107202c1af564590775d3e5b350c350afc8
mmendesc <matheus.mendescf@gmail.com>
2017-02-21 10:22:20 -0300

    Implemented user connection to posts with tests

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,6 +15,8 @@ describe 'Navigate' do
 
   describe "creation" do
     before do
+      user = User.create(email: 'teste@test.com', password: '123456', password_confirmation: '123456', first_name: 'Jon',last_name: 'Doe')
+      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
@@ -32,6 +34,15 @@ describe 'Navigate' do
 
       expect(page).to have_content("Some rationale")
     end
+
+    it 'will have a user associated' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: 'User_Association'
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User_Association")
+    end
+
   end
 
 end
