e6582293180560831adf9ab97bf9bf0a1b232302
mmendesc <matheus.mendescf@gmail.com>
2017-02-22 10:28:30 -0300

    Refactor index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: 'teste@test.com', password: '123456', password_confirmation: '123456', first_name: 'Jon', last_name: 'Doe')
+puts "User created"
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content",user_id: @user.id)
 end
 
 puts "100 posts created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,22 +1,36 @@
 require 'rails_helper'
 
 describe 'Navigate' do
+  before do
+    @user = User.create(email: 'teste@test.com', password: '123456', password_confirmation: '123456', first_name: 'Jon',last_name: 'Doe')
+    login_as(@user, :scope => :user)
+  end
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+    it 'can be reached successfully' do
+
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: 'Anything 1',user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: 'Anything 2',user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Anything 1|Anything 2/)
+    end
+
+
   end
+
 
   describe "creation" do
     before do
-      user = User.create(email: 'teste@test.com', password: '123456', password_confirmation: '123456', first_name: 'Jon',last_name: 'Doe')
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
